f9c626c7cb3795683da75e26947a3b6e40c90e8d
el-khafifi <elkhafifizakaria@gmail.com>
2023-02-09 10:04:10 +0100

    updated route module.closes #6

--- miniPro/src/app/app-routing.module.ts
+++ miniPro/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookCreateComponent } from './books/book-create/book-create.component';
+import { BookListComponent } from './books/book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:BookListComponent},
+  {path:'create',component:BookCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
