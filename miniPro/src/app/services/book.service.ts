d2b5448856b7866ec528feb3a43bdfb4ab9691e1
el-khafifi <elkhafifizakaria@gmail.com>
2023-02-09 10:11:57 +0100

    book initialised with fake data.closes #8

--- miniPro/src/app/services/book.service.ts
+++ miniPro/src/app/services/book.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Book } from '../models/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
+  books:Book[]=[{title:"bookTitle",content:"bookContent"}]
+
   constructor() { }
+
 }
