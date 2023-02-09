c06999b1b708144a3c4cecae0262538af8666364
el-khafifi <elkhafifizakaria@gmail.com>
2023-02-09 09:47:07 +0100

    created components.closes #4

--- miniPro/src/app/app.module.ts
+++ miniPro/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './books/header/header.component';
+import { BookCreateComponent } from './books/book-create/book-create.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookItemComponent } from './books/book-list/book-item/book-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookCreateComponent,
+    BookListComponent,
+    BookItemComponent
   ],
   imports: [
     BrowserModule,
